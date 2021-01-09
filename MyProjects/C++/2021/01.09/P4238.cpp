50f15af1926ee27c09c67d6b3fb81a78edbfd7bd
yanyanlongxia <59304287+yanyanlongxia@users.noreply.github.com>
2021-01-09 12:33:59 +0800

    Update

---
+++ MyProjects/C++/2021/01.09/P4238.cpp
@@ -0,0 +1,14 @@
+//File: P4238.cpp
+//Author: yanyanlongxia
+//Date: 2021/1/9
+//
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main() {
+    freopen("data.in","r",stdin);
+    //freopen("P4238.out","w",stdout);
+
+}
---
+++ MyProjects/C++/2021/01.09/cmake-build-debug/CMakeFiles/P4238.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P4238.dir/P4238.cpp.o"
+  "P4238"
+  "P4238.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P4238.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
