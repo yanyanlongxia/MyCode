eac243bda06b3905e81b13bcd1e1065fa53f4b8a
DELL <yanyanlongxia@outlook.com>
2021-02-01 10:35:19 +0800

    Update

---
+++ MyProjects/C++/2021/02.01/cmake-build-debug/CMakeFiles/02_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02_01"
+  "02_01.pdb"
+  "CMakeFiles/02_01.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
