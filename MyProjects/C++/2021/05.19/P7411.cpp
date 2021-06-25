20de26e2662d7eb2ff9fe0bb8166503d29b77af2
DELL <yanyanlongxia@outlook.com>
2021-06-25 13:55:46 +0800

    Update

--- MyProjects/C++/2021/05.19/P7411.cpp
+++ MyProjects/C++/2021/05.19/P7411.cpp
@@ -12,15 +12,47 @@
 using namespace std;
 const int N=1e6,M=5e3;
 const int dx[4]={1,0,0,-1},dy[4]={0,1,-1,0};
-int n,head,tail;
+int n,head,tail,ans;
 pair<int,int>q[N];
 bool valid[M][M];
-void checkadd(int x,int y){
-
+void checkadd(int x,int y) {
+    if (!valid[x][y])
+        return;
+    int nu = 0;
+    for (int i = 0; i < 4; i++)
+        if (valid[x + dx[i]][y + dy[i]])
+            nu++;
+    if (nu == 3) {
+        for (int i = 0; i < 4; i++) {
+            pair<int,int>nxt=make_pair(x+dx[i],y+dy[i]);
+            if (!valid[nxt.first][nxt.second])
+                q[++tail]=nxt;
+        }
+    }
 }
 int main() {
     freopen("data.in","r",stdin);
     //freopen("P7411.out","w",stdout);
-
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        //ans=0;
+        pair<int,int>now;
+        scanf("%d%d",&now.first,&now.second);
+        now.first+=1000,now.second+=1000;
+        head=1,tail=0;
+        q[++tail]=now;
+        while(tail>=head){
+            now=q[head];
+            head++;
+            if (valid[now.first][now.second])
+                continue;
+            valid[now.first][now.second]=1;
+            ans++;
+            checkadd(now.first,now.second);
+            for(int j=0;j<4;j++)
+                checkadd(now.first+dx[j],now.second+dy[j]);
+        }
+        printf("%d\n",ans-i);
+    }
     return 0;
 }
