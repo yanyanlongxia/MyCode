7ce6d62afad46065b8039e6832857084688e71a7
DELL <yanyanlongxia@outlook.com>
2021-06-28 15:05:26 +0800

    Update

--- MyProjects/C++/2021/06.28/P2163.cpp
+++ MyProjects/C++/2021/06.28/P2163.cpp
@@ -10,7 +10,7 @@
 #define infm(x) memset(x,0x3f3f3f3f,sizeof(x))
 #define minfm(x) memset(x,0xcf,sizeof(x))
 using namespace std;
-const int N=2e7;
+const int N=1e7;
 vector<int>v1,v2;
 int n,m,t[N],ans[N];
 struct tree{
@@ -29,7 +29,7 @@ struct ques{
     }
 };
 inline void add(int x,int d){
-    for(;x<=n;x+=(x & -x))
+    for(;x<=v2.size();x+=(x & -x))
         t[x]+=d;
 }
 inline int ask(int x){
@@ -40,10 +40,10 @@ inline int ask(int x){
 }
 vector<ques>q;
 inline int get_num1(int x){
-    return lower_bound(v1.begin(),v1.end(),x)-v1.begin();
+    return lower_bound(v1.begin(),v1.end(),x)-v1.begin()+1;
 }
 inline int get_num2(int x){
-    return lower_bound(v2.begin(),v2.end(),x)-v2.begin();
+    return lower_bound(v2.begin(),v2.end(),x)-v2.begin()+1;
 }
 int main() {
     freopen("data.in","r",stdin);
@@ -52,19 +52,18 @@ int main() {
     int x,y,a,b,c,d;
     for(int i=1;i<=n;i++){
         scanf("%d%d",&x,&y);
-        /*v1.push_back(x);v2.push_back(y);*/
+        v1.push_back(x);v2.push_back(y);
         node[i].x=x;node[i].y=y;
     }
     for(int i=1;i<=m;i++){
         scanf("%d%d%d%d",&a,&b,&c,&d);
-        /*v1.push_back(a);v2.push_back(b);v1.push_back(a-1);v2.push_back(b-1);
-        v1.push_back(c);v2.push_back(d);v1.push_back(c-1);v2.push_back(d-1);*/
+        v1.push_back(a);v2.push_back(b);v1.push_back(a-1);v2.push_back(b-1);
+        v1.push_back(c);v2.push_back(d);/*v1.push_back(c-1);v2.push_back(d-1);*/
         q.push_back(ques(c,d,i,1));
         q.push_back(ques(a-1,b-1,i,1));
         q.push_back(ques(c,b-1,i,-1));
         q.push_back(ques(a-1,d,i,-1));
     }
-    /*
     sort(v1.begin(),v1.end());
     sort(v2.begin(),v2.end());
     v1.erase(unique(v1.begin(),v1.end()),v1.end());
@@ -73,21 +72,8 @@ int main() {
         node[i].x=get_num1(node[i].x),node[i].y=get_num2(node[i].y);
     for(int i=0;i<q.size();i++)
         q[i].x=get_num1(q[i].x),q[i].y=get_num2(q[i].y);
-        */
     sort(node+1,node+1+n);
     sort(q.begin(),q.end());
-    /*
-    bool pppp=false;
-    for(int i=0;i<v1.size();i++){
-        if (i!=v1[i]){
-            pppp=true;
-            break;
-        }
-    }
-    if (pppp)
-        printf("1");
-*/
-
     int j=0;
     for(int i=0;i<q.size();i++){
         while (node[j+1].x<=q[i].x&&j+1<=n){
@@ -99,6 +85,5 @@ int main() {
     }
     for(int i=1;i<=m;i++)
         printf("%d\n",ans[i]);
-
     return 0;
 }
