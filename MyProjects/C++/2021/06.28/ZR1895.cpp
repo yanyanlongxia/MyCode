6f78dc91af9a01a62f5e0f3db780de5023fe43c5
DELL <yanyanlongxia@outlook.com>
2021-06-29 16:03:30 +0800

    Update

--- MyProjects/C++/2021/06.28/ZR1895.cpp
+++ MyProjects/C++/2021/06.28/ZR1895.cpp
@@ -13,6 +13,7 @@ using namespace std;
 const int N=3e3;
 int n,m;
 char s[N][N];
+bool vis[N][N];
 vector<pair<int,int> >cu,nxt;
 int main() {
     freopen("data.in","r",stdin);
@@ -22,6 +23,40 @@ int main() {
     for(int i=1;i<=n;i++)
         scanf("%s",(s[i]+1));
     cu.push_back(make_pair(1,1));
+    char mn=s[1][1];
+    for(;!cu.empty();cu=nxt) {
+        printf("%c",mn);
+        mn='z'+1;
+        for (auto i:cu) {
+            int dx = 1, dy = 0;
+            for (int j = 1; j <= 2; j++) {
+                swap(dx, dy);
+                int nx = i.first + dx;
+                int ny = i.second + dy;
+                if (nx > n || ny > m)
+                    continue;
+                mn = min(mn, s[nx][ny]);
+            }
+        }
+        nxt.clear();
+
+        for (auto i:cu) {
+            int dx = 1, dy = 0;
+            for (int j = 1; j <= 2; j++) {
+                swap(dx, dy);
+                int nx = i.first + dx;
+                int ny = i.second + dy;
+                if (nx > n || ny > m)
+                    continue;
+                if (vis[nx][ny])
+                    continue;
+                if (s[nx][ny] == mn) {
+                    nxt.push_back(make_pair(nx, ny));
+                    vis[nx][ny] = true;
+                }
+            }
+        }
+    }
 
     return 0;
 }
