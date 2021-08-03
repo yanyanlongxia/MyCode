978592c3d88e2023c12c581f5cf1a6458480298f
yanyanlongxia <yanyanlongxia@outlook.com>
2021-08-03 10:22:58 +0800

    Update

---
+++ MyProjects/C++/2021/08.03/cmake-build-debug/CMakeFiles/08_03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "08_03"
+  "08_03.pdb"
+  "CMakeFiles/08_03.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/08_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
